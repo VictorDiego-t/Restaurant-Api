d0f8d286c301666030fe897428e006072a63accf
Victor Diego <studyvictordiego@gmail.com>
2025-02-20 06:16:06 -0300

    Repositories

--- src/main/java/com/victordiego/api_restaurante/repositories/CategoryRepository.java
+++ src/main/java/com/victordiego/api_restaurante/repositories/CategoryRepository.java
@@ -1,4 +1,10 @@
 package com.victordiego.api_restaurante.repositories;
 
-public class CategoryRepository {
+import com.victordiego.api_restaurante.domain.category.Category;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends MongoRepository<Category, String> {
+
 }
--- src/main/java/com/victordiego/api_restaurante/repositories/ProductRepository.java
+++ src/main/java/com/victordiego/api_restaurante/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.victordiego.api_restaurante.repositories;
 
-public class ProductRepository {
+import com.victordiego.api_restaurante.domain.products.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
